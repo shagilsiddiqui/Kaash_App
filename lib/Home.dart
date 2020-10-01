7d8783beec189dc2efd5abccf1e8a7e2c7ac175b
Sidhant Goyal <sidhant702@gmail.com>
2020-10-01 18:44:28 +0530

    CHanges

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:kaash_app/intro.dart';
+import 'package:kaash_app/permiServices.dart';
 import 'package:kaash_app/shared/buttons.dart';
 import 'package:kaash_app/shared/styles.dart';
 import 'package:page_transition/page_transition.dart';
@@ -10,6 +11,9 @@ class home extends StatefulWidget {
 }
 
 class _homeState extends State<home> {
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,10 +30,14 @@ class _homeState extends State<home> {
               Container(
                 width: 200,
                 margin: EdgeInsets.only(bottom: 0),
-                child: froyoFlatBtn('Enter', (){
-
-                 Navigator.pushReplacement(context, PageTransition(type: PageTransitionType.rotate, duration: Duration(seconds: 1),  child:intro()));
-                }),
+                child: froyoFlatBtn('Enter', () async {
+                   bool s = await PermissionsService().requeststoragePermission(); 
+                  bool v=  await PermissionsService().requevoicePermission();
+                  if( v && s) {
+                    print(v.toString());
+                  Navigator.pushReplacement(context, PageTransition(type: PageTransitionType.rotate, duration: Duration(milliseconds: 10),  child:intro()));
+                  }
+                }  ),
               ),
             ],
           )),
